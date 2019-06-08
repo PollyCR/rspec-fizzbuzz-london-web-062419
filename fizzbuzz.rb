ffac272841c1af7317cc43e72323e2e62304f031
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-08 12:40:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  if number % 15
+    return "FizzBuzz"
+    elsif number % 5 
+    return "Buzz"
+    elsif number % 3
+    return "Fizz"
+  else return NIL
+end
+  
+end
