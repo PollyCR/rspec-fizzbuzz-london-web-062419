820a8f72cd9f30819f592c494a275164120262fe
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-08 13:03:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,12 @@
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
     return "FizzBuzz"
-    elsif number % 5 && number % 3 != 0
+    elsif number % 5 == 0  && number % 3 != 0
     return "Buzz"
-    elsif number % 3 && number % 5 != 0
+    elsif number % 3 == 0 && number % 5 != 0
     return "Fizz"
-  else return nil
+  elsif number % 3 != 0 && number % 5 != 0 
+  return nil
 end
   
 end
